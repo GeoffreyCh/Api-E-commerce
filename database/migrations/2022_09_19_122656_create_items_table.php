47e20b2db319f543a68fc2d9e594dbc54f85ad21
GeoffreyCh <gc.geoffrey.c@gmail.com>
2022-09-19 15:28:13 +0200

    test

--- database/migrations/2022_09_19_122656_create_items_table.php
+++ database/migrations/2022_09_19_122656_create_items_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->text('description');
             $table->decimal('price');
             $table->string('image_url');
-            //$table->timestamps();
         });
     }
 
