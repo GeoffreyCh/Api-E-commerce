f78250631af5078a53c5721d81e391d7a1a29ef7
Nicolas REITIN <contact@nicolas-reitin.fr>
2022-09-20 17:01:41 +0200

    update laravel sanctum not finish

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(),
+        [
+            'name' => 'required|string|max:255',
+            'pseudo'=>'required|string|max:255', 
+            'email'=>'required|email|unique:users',
+            'password'=>'required',
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Validation failed',
+                'errors'=>$validate->errors()
+            ], 401);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'pseudo' => $request->pseudo,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Utilisateur créé',
+            'token'=> $user->createToken('Token')->plainTextToken
+        ], 200);
+        // $validatedData = $request->validate([
+        //     'name' => 'required|string|max:255',
+        //     'pseudo'=>'required|string|max:255',
+        //     'email' => 'required|email|max:255',
+        //     'password' => 'required|string|min:8'
+        // ]);
+
+        // $user = User::create([
+        // 'name' => $validatedData['name'],
+        // 'pseudo' => $validatedData['pseudo'],
+        // 'email' => $validatedData['email'],
+        // 'password' => Hash::make($validatedData['password']),
+        // ]);
+
+
+        // $token = $user->createToken('auth_token')->plainTextToken;
+
+        // return response()->json([
+        //     'access_token' => $token,
+        //     'token_type' => 'Bearer',
+        // ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+        'message' => 'Invalid login details'
+                ], 401);
+            }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'message' => 'Utilisateur connecté',
+                'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function logout(Request $request)
+    {
+        return $request->user()->currentAccessToken()->delete();
+    }
+}
