fdffb3b60af674a5f3379a18c3998f4477be7c8e
GeoffreyCh <gc.geoffrey.c@gmail.com>
2022-09-20 11:36:54 +0200

    seeders faker

--- database/seeders/ItemsSeeder.php
+++ database/seeders/ItemsSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Text;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Decimal;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ItemsSeeder extends Seeder
@@ -20,11 +21,16 @@ class ItemsSeeder extends Seeder
     public function run()
     {
         $faker = Factory::create();
+        // $filePath = storage_path('images');
+
+        // if(!File::exists($filePath)){
+        //     File::makeDirectory($filePath);
+        // }
 
         DB::table('items')->insert([
-            'title' => Str::random(8),
-            'description' => Str::random(50),
-            'image_url' => Str::random(10),
+            'title' => $faker->word,
+            'description' => $faker->text(200),
+            'image_url' => $faker->imageUrl(400, 300),
             'price' => $faker->randomFloat(2, 0, 100),
         ]);
     }
